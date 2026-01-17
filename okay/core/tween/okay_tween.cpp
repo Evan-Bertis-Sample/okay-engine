346d669d9ca98bb904605eb89a2dd7bb625de449
kodeti <kiritortino@gmail.com>
2026-01-17 14:15:38 -0600

    add easing enums

--- okay/core/tween/okay_tween.cpp
+++ okay/core/tween/okay_tween.cpp
@@ -3,3 +3,12 @@
 #include <okay/core/logging/okay_logger.hpp>
 
 using namespace okay;
+
+namespace okay {
+
+enum OkayTweenEasingStyle : std::uint8_t { LINEAR, SINE, CUBIC, QUAD, QUINT, CIRC, ELASTIC, QUART, EXPO, BACK, BOUNCE };
+enum OkayTweenEasingDirection : std::uint8_t { IN, OUT, INOUT };
+
+}; // namespace okay
+
+
