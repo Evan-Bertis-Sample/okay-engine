ed4543757aef55975a8a647a745ea82530d46204
kodeti <kiritortino@gmail.com>
2026-02-24 19:59:59 -0600

    comment out okay imgui tick

--- okay/core/renderer/imgui/okay_imgui.cpp
+++ okay/core/renderer/imgui/okay_imgui.cpp
@@ -26,37 +26,37 @@ void OkayIMGUI::postInitialize() {}
 void OkayIMGUI::tick() {
     // Rendering
     // (Your code clears your framebuffer, renders your other stuff etc.)
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+    // ImGui_ImplOpenGL3_NewFrame();
+    // ImGui_ImplGlfw_NewFrame();
+    // ImGui::NewFrame();
     
-    ImGui::Begin("Properties");
-    if (ImGui::CollapsingHeader("test", ImGuiTreeNodeFlags_DefaultOpen)) {
-        if (ImGui::Button("testButton")) {
-            okay::Engine.logger.debug("aaa");
-        }
-    }
-    float placeholder {};
-    bool b1 {};
-    bool b2 {};
-    bool b3 {};
-    if (ImGui::CollapsingHeader("stuff")) {
-        ImGui::Text("Name");
-        ImGui::ColorPicker3("Color", &placeholder, ImGuiColorEditFlags_PickerHueWheel);
-        ImGui::SliderFloat("Metallic", &placeholder, 0.0f, 1.0f);
-        ImGui::Separator();
-        if (ImGui::BeginTable("split", 3))
-        {
-            ImGui::TableNextColumn(); ImGui::Checkbox("No titlebar", &b1);
-            ImGui::TableNextColumn(); ImGui::Checkbox("No scrollbar", &b2);
-            ImGui::TableNextColumn(); ImGui::Checkbox("No menu", &b3);
-            ImGui::EndTable();
-        }
-    }
-    ImGui::End();
+    // ImGui::Begin("Properties");
+    // if (ImGui::CollapsingHeader("test", ImGuiTreeNodeFlags_DefaultOpen)) {
+    //     if (ImGui::Button("testButton")) {
+    //         okay::Engine.logger.debug("aaa");
+    //     }
+    // }
+    // float placeholder {};
+    // bool b1 {};
+    // bool b2 {};
+    // bool b3 {};
+    // if (ImGui::CollapsingHeader("stuff")) {
+    //     ImGui::Text("Name");
+    //     ImGui::ColorPicker3("Color", &placeholder, ImGuiColorEditFlags_PickerHueWheel);
+    //     ImGui::SliderFloat("Metallic", &placeholder, 0.0f, 1.0f);
+    //     ImGui::Separator();
+    //     if (ImGui::BeginTable("split", 3))
+    //     {
+    //         ImGui::TableNextColumn(); ImGui::Checkbox("No titlebar", &b1);
+    //         ImGui::TableNextColumn(); ImGui::Checkbox("No scrollbar", &b2);
+    //         ImGui::TableNextColumn(); ImGui::Checkbox("No menu", &b3);
+    //         ImGui::EndTable();
+    //     }
+    // }
+    // ImGui::End();
 
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // ImGui::Render();
+    // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     // (Your code calls glfwSwapBuffers() etc.)
 }
 
