5f80728d6035ddceaef04f7476c4238de88e15f9
evan-bertis-sample <esample21@gmail.com>
2026-03-26 20:45:08 -0400

    Readd #include <glad/gl.h> in glad.c

--- okay/platform/windows/glfw_surface.cpp
+++ okay/platform/windows/glfw_surface.cpp
@@ -5,7 +5,6 @@
 
 #include <iostream>
 #include <stdexcept>
-#include <string>
 
 namespace okay {
 
--- okay/vendor/glad/src/glad.c
+++ okay/vendor/glad/src/glad.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <glad/glad.h>
 
 static void* get_proc(const char *namez);
 
@@ -1671,4 +1671,3 @@ int gladLoadGLLoader(GLADloadproc load) {
 	if (!find_extensionsGL()) return 0;
 	return GLVersion.major != 0 || GLVersion.minor != 0;
 }
-
